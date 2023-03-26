5066ece7789b617449624fbacb6474f5f6adfffd
GustavoVzla <gustavo.tejera@hotmail.com>
2023-03-26 15:02:33 +0200

    primera rama gustavo

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 "use stric";
 
-
 //Array de palabras a descubrir
 const palabras = [
-    "patata",
-    "pimiento",
-    "berenjena",
-    "coliflor",
-    "brocoli",
-    "acelgas",
-    "calabaza"
-]
+  "patata",
+  "pimiento",
+  "berenjena",
+  "coliflor",
+  "brocoli",
+  "acelgas",
+  "calabaza",
+];
 
 //Generar palabra aleatoria
 
 const palabraRandom = Math.floor(Math.random() * palabras.length);
-const palabraSecreta = palabras[palabraRandom]
-console.log(palabraSecreta)
+const palabraSecreta = palabras[palabraRandom];
+console.log(palabraSecreta);
 
 //Palabra Oculta
 const palabraOculta = palabraSecreta.replace(/./g, "_");
@@ -25,10 +24,28 @@ const palabraOculta = palabraSecreta.replace(/./g, "_");
 const h2 = document.getElementById("palabraSecreta");
 h2.textContent = palabraOculta;
 
-
-
 const letraSeleccionada = document.getElementById("letra");
-const comprobar = document.getElementById("comprobar")
-const reset = document.getElementById("reset")
+const comprobar = document.getElementById("comprobar");
+const reset = document.getElementById("reset");
 
-addEventListener
+comprobar.addEventListener("click", () => {
+  const palabraSecretaArray = palabraSecreta.split("");
+
+  if (palabraSecretaArray.includes(letraSeleccionada.value)) {
+    const palabraOcultaArray = palabraOculta.split("");
+
+    let palabraVisible;
+
+    for (let i = 0; i <= palabraSecretaArray.length - 1; i++) {
+      if (palabraSecretaArray[i] === letraSeleccionada.value) {
+        palabraOcultaArray[i] = letraSeleccionada.value;
+        console.log(palabraOcultaArray);
+
+        palabraVisible = palabraOcultaArray.join("");
+        console.log(palabraVisible);
+
+        h2.textContent = palabraVisible;
+      }
+    }
+  }
+});
