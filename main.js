05d61716151892c703795ee20073f038734ea7af
GustavoVzla <gustavo.tejera@hotmail.com>
2023-03-27 13:17:07 +0200

    segundo commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-"use stric";
+"use strict";
 
-//Array de palabras a descubrir
 const palabras = [
   "patata",
   "pimiento",
@@ -11,18 +10,20 @@ const palabras = [
   "calabaza",
 ];
 
-//Generar palabra aleatoria
-
+// Generar palabra aleatoria
 const palabraRandom = Math.floor(Math.random() * palabras.length);
-const palabraSecreta = palabras[palabraRandom];
+let palabraSecreta = palabras[palabraRandom];
 console.log(palabraSecreta);
 
-//Palabra Oculta
-const palabraOculta = palabraSecreta.replace(/./g, "_");
+// Palabra oculta
+let palabraOculta = palabraSecreta.replace(/./g, "_");
 
-//Mostrar palabra en pantalla
+// Mostrar palabra en pantalla
 const h2 = document.getElementById("palabraSecreta");
 h2.textContent = palabraOculta;
+
+// Contador de fallos
+let fallos = 0;
 
 const letraSeleccionada = document.getElementById("letra");
 const comprobar = document.getElementById("comprobar");
@@ -47,5 +48,41 @@ comprobar.addEventListener("click", () => {
         h2.textContent = palabraVisible;
       }
     }
+
+    // Comprobar si se ha adivinado la palabra
+    if (palabraVisible === palabraSecreta) {
+      alert(`Has ganado! La palabra era ${palabraSecreta}`);
+      comprobar.disabled = true;
+    }
+  } else {
+    // Aumentar el contador de fallos
+    fallos++;
+
+    // Comprobar si se han agotado los intentos
+    if (fallos === 6) {
+      alert(`Has perdido! La palabra era ${palabraSecreta}`);
+      comprobar.disabled = true;
+    }
   }
+
+  // Limpiar el campo de la letra
+  letraSeleccionada.value = "";
+});
+
+reset.addEventListener("click", () => {
+  // Generar una nueva palabra aleatoria
+  const nuevaPalabraRandom = Math.floor(Math.random() * palabras.length);
+  palabraSecreta = palabras[nuevaPalabraRandom];
+  console.log(palabraSecreta);
+
+  // Ocultar la nueva palabra
+  palabraOculta = palabraSecreta.replace(/./g, "_");
+  h2.textContent = palabraOculta;
+
+  // Reiniciar el contador de fallos
+  fallos = 0;
+  h3.textContent = `Fallos: ${fallos}`;
+
+  // Habilitar el botón de comprobar
+  comprobar.disabled = false;
 });
